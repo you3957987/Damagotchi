62feab63cf08d816cdbbc19eca9c409c2aff3832
Vice <73925257+you3957987@users.noreply.github.com>
2024-11-11 14:49:08 +0900

    DADADA
    A

--- LoadingFunction.js
+++ LoadingFunction.js
@@ -16,5 +16,24 @@ function fade_out(nextpage){
 	setTimeout(() => {
 		window.location.href = nextpage; // 다음 페이지로 이동
     }, 1000); // 1초 후에 페이지 이동
+}
 
+function good_ending_out(nextpage){
+    const good_ending_out = document.getElementById('good_ending_out');
+    const fade_in = document.getElementById('fade_in');
+    good_ending_out.style.zIndex = '9999'; // 오버레이를 맨 앞으로 이동
+    good_ending_out.style.opacity = '1'; // 오버레이를 검게 만들기
+    setTimeout(() => {
+        window.location.href = nextpage; // 다음 페이지로 이동
+    }, 2000); // 1초 후에 페이지 이동
+}
+
+function bad_ending_out(nextpage){
+    const bad_ending_out = document.getElementById('bad_ending_out');
+    const fade_in = document.getElementById('fade_in');
+    bad_ending_out.style.zIndex = '9999'; // 오버레이를 맨 앞으로 이동
+    bad_ending_out.style.opacity = '1'; // 오버레이를 검게 만들기
+    setTimeout(() => {
+        window.location.href = nextpage; // 다음 페이지로 이동
+    }, 2000); // 1초 후에 페이지 이동
 }
